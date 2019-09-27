78f11ea027eceaa7ac10e1d4f295cce7b822e09a
Chukwu Gideon <chuksgid@gmail.com>
2019-09-27 13:23:07 +0100

    Total Expenses UI Design

---
+++ mobile/app/src/main/java/com/chegsmania/triptotracker/views/activities/DashboardActivity.kt
@@ -0,0 +1,13 @@
+package com.chegsmania.triptotracker.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chegsmania.triptotracker.R
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
